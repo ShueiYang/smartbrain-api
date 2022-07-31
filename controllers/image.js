89620164e4b11da8906d5a03457d6501240e24bc
ShueiYang <yangshuei83@gmail.com>
2022-07-31 23:17:26 +0200

    update connection to API

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,7 @@ const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key 76dcba01969d4933a94b86609a68d0cb");
+metadata.set("authorization", "Key", process.env.CLARIFAI_API);
 
 const handleApiCall = (req, resp) => {
     stub.PostModelOutputs(
