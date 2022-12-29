855f1501a70c2eb0e2e106e0d0852f9c9c7543bf
ShueiYang <yangshuei83@gmail.com>
2022-12-29 23:09:35 +0100

    add google Oauth2.0 for logging

---
+++ controllers/auth.profile.js
@@ -0,0 +1,20 @@
+
+const handleGoogleProfile = (req, res, database) => {
+    const { email } = req.body
+
+    database.select('*')
+            .from('users')
+            .where('email', '=', email)
+            .then(user => {
+                if (user.length) {
+                    res.json(user[0])
+                } else {
+                    res.status(404).json('User Not Found')
+                }
+            })
+            .catch(err => res.status(400).json(`${err}`))
+}
+
+module.exports = {
+    handleGoogleProfile
+}
--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const cors = require('cors');
 const helmet = require("helmet");
 const passport = require("passport");
 const googleStrategy = require("passport-google-oauth20").Strategy;
+const session = require('express-session');
 
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
+const { handleRegister } = require('./controllers/register');
+const { handleSignin } = require('./controllers/signin');
+const { handleGoogleProfile } = require('./controllers/auth.profile');
+const { handleApiCall, handleImage } = require('./controllers/image');
+
 
 const database = knex({
     client: 'pg',
@@ -27,19 +29,48 @@ const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+}));
 
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
-    // COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-    // COOKIE_KEY_2: process.env.COOKIE_KEY_2
+    COOKIE_SECRET: process.env.COOKIE_SECRET,
 };
 
-function verifiyCallback(accessToken, refreshToken, profile, done) {
-    console.log('Google profile', profile.displayName);
-    
+app.use(session({
+    secret: config.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 2 }
+}));
+
+
+ async function verifiyCallback(accessToken, refreshToken, profile, done) {
+    try{
+        await database.transaction(async trx => {
+            const result = await trx("oauth").where("profile_id", "=", profile.id);
+            if(result.length === 0) {
+                const googleEmail = await trx("oauth")
+                    .insert({
+                        profile_id: profile.id,
+                        email: profile._json.email
+                    })
+                    .returning("email");
+                
+                await trx("users").insert({
+                    name: profile._json.name,
+                    email: googleEmail[0].email,
+                    joined: new Date()
+                })
+            };
+        })
+    } catch (err) {
+        console.error(err)
+    };
     done(null, profile);
 };
 
@@ -56,18 +87,24 @@ passport.serializeUser((user, done) => {
     done(null, user);
 });
 
-passport.deserializeUser((id, done) => {
-    // User.findbyId(id).then(user => {
-    //     done(null, user);
-    // })
-    done(null, id);
+passport.deserializeUser((user, done) => {
+    done(null, user);
 })
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 
 
 
+function checkLoggedIn(req, res, next) {
+    
+    const isLoggedIn = req.isAuthenticated() && req.user;
+    if(!isLoggedIn) {
+        return res.status(204).send();  //No need to send information
+    }
+    next();
+};
 
 
 app.get('/', (req, res) => {
@@ -75,37 +112,53 @@ app.get('/', (req, res) => {
 })
 
 app.get("/auth/google", 
-    passport.authenticate("google", { scope: ["profile"] }));
+    passport.authenticate("google", { scope: ["email", "profile"]}));
 
 app.get("/auth/google/callback", 
     passport.authenticate("google", {
-        failureRedirect: '/failure',
-        successRedirect: "/home",
-        session: false
+        failureRedirect: "/failure",
     }),
     (req, res) => {
-        console.log("Google called us back !")
-    });
+        res.redirect("http://localhost:3000");
+    }
+);
+
+app.get('/auth/login', checkLoggedIn, (req, res) => {
+    res.status(200).json(req.user);
+});
 
 
 
+app.get("/auth/logout", (req, res, next) => {
+    //Remove req.user and clears any logged in session
+    req.logout(function (err) {   
+        if(err) {
+            return next(err);
+        }
+        res.status(204).send();
+    }); 
+});
 
 
+app.post('/signin', (req, res) => {
+    handleSignin(req, res, database, bcrypt)
+});
 
-app.post('/signin', (req, res) =>
-{signin.handleSignin(req, res, database, bcrypt)})
-
-app.post('/register', (req, res) => 
-{register.handleRegister(req, res, database, bcrypt, saltRounds)})        
+app.post('/register', (req, res) => {
+    handleRegister(req, res, database, bcrypt, saltRounds)
+});        
             
-app.get('/profile/:id', (req, res) => 
-{profile.handleProfileGet(req, res, database)})
+app.post('/auth/profile', (req, res) => {
+    handleGoogleProfile(req, res, database)
+});
 
-app.put('/image', (req, res) => 
-{image.handleImage(req, res, database)})
+app.post('/imageurl', (req, res) => {
+    handleApiCall(req, res)
+});
 
-app.post('/imageurl', (req, res) => 
-{image.handleApiCall(req, res)})
+app.put('/image', (req, res) => {
+    handleImage(req, res, database)
+});
 
 
 const PORT = process.env.PORT || 8080;
