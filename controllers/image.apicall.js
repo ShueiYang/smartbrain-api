0d7a69298ad12608d5bce22cdb571856db1603dc
ShueiYang <yangshuei83@gmail.com>
2023-01-15 19:11:17 +0100

    fix and update face-detection model

--- controllers/image.apicall.js
+++ controllers/image.apicall.js
@@ -11,26 +11,27 @@ const handleApiCall = (req, res) => {
     stub.PostModelOutputs(
         {
             // This is the model ID of a FACE DETECT model. We may use any other public or custom model ID.
-            model_id: "a403429f2ddf4b49b307e318f00e528b",
+            model_id: "face-detection",
+            version_id: "6dc7e46bc9124c5c8824be4822abe105",
             inputs: [{data: {image: {url: req.body.input }}}]
         },
         metadata,
         (err, response) => {
             if (err) {
                 console.log("Unable to work with API: " + err);
-                return res.status(503).json("Unable to work with API: " + err)
+                return res.status(503).json({message: `Unable to work with API: ${err}`})
             }
 
             if (response.status.code !== 10000) {
                 console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
-                return res.status(404).json("Image not found")
+                return res.status(404).json({message: `Error: ${response.status.description}`})
             }
 
             console.log("Predicted concepts, with confidence values:")
             for (const c of response.outputs[0].data.concepts) {
                 console.log(c.name + ": " + c.value);
             }
-            res.json(response)
+            res.json(response);
         }
     );
 };
